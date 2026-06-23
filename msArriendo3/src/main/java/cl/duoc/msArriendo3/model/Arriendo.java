911f4e9e228e1dd41d57821dbc092e94da50c86f
bnmnnm <be.curinanco@duocuc.cl>
2026-06-23 19:55:38 -0400

    Documentacion Model

--- msArriendo3/src/main/java/cl/duoc/msArriendo3/model/Arriendo.java
+++ msArriendo3/src/main/java/cl/duoc/msArriendo3/model/Arriendo.java
@@ -2,6 +2,7 @@ package cl.duoc.msArriendo3.model;
 
 import java.util.Date;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,17 +21,22 @@ import lombok.NoArgsConstructor;
 public class Arriendo {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Schema(description = "Identificador único del arriendo", example = "1")
     private Integer id;
     
     @Column(nullable = false)
+    @Schema(description = "Fecha de inicio del arriendo", example = "2023-01-01")
     private Date fechaInicio;
     
     @Column(nullable = false)
+    @Schema(description = "Fecha de fin del arriendo", example = "2023-01-01")
     private Date fechaFin;
 
     @Column(name = "reserva_id", nullable = false)
+    @Schema(description = "Identificador de la reserva asociada", example = "1")
     private Integer reservaId;
 
     @Column(name = "empleado_id", nullable = false)
+    @Schema(description = "Identificador del empleado encargado", example = "1")
     private Integer empleadoId;
 }
